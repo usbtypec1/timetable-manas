7b801c768d2ea79529b1a62692e8c296446a946e
Eldos <eldos.baktybekov@gmail.com>
2024-09-02 19:48:21 +0600

    :monocle_face: Add Google Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,17 @@ import Aura from '@primevue/themes/aura'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [
-    '@primevue/nuxt-module',
-  ],
+  modules: ['@primevue/nuxt-module', 'nuxt-gtag'],
   primevue: {
     options: {
       theme: {
         preset: Aura
       }
     }
+  },
+
+  gtag: {
+    id: 'G-DYXDM94GJP',
   },
 
   ssr: true,
